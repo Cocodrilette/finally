d54ef2665dd34fa6df74a92b5ddc23e8754f9f63
cocodrilette <cocodrilette@gmail.com>
2024-11-09 15:39:32 -0500

    Refactor sign-in and sign-up pages for improved layout and spacing

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center p-auto m-10 h-[571px]">
+    <div className="flex items-center justify-center pt-5 m-auto h-[571px]">
       <SignUp />
     </div>
   );
