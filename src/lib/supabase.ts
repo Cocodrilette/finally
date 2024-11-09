5b821e1c2c3b269eb6044811c3cd7e1132eee5cc
cocodrilette <cocodrilette@gmail.com>
2024-11-09 15:23:25 -0500

    Improve error handling in Supabase configuration by separating checks for Supabase URL and Anon Key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,8 +4,12 @@ import { Database } from "../../database.types";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase URL or Supabase Anon Key");
+if (!supabaseUrl) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
