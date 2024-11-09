4974e3de2312e66bee3b22b4a5884475e98b3b47
cocodrilette <cocodrilette@gmail.com>
2024-11-09 11:21:35 -0500

    Update database URL in drizzle config and implement user fetching in Main component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -6,6 +6,9 @@ import { AddRecordView } from "./add-record-view";
 import { HomeView } from "./home-view";
 import { useAuth } from "@clerk/nextjs";
 import { useEffect } from "react";
+import { usersTable } from "@/db/schema";
+import { db } from "@/db";
+import { sql } from "drizzle-orm";
 
 export function Main() {
   const { isLoaded: isUserLoaded, userId } = useAuth();
@@ -13,9 +16,28 @@ export function Main() {
   const searchParams = useSearchParams();
   const tab = searchParams.get("tab") || "home";
 
+  async function fetchUser() {
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(sql`id = ${userId}`);
+
+    if (user) {
+      console.log("User is logged in");
+    } else {
+      console.log("User is not in the database");
+    }
+  }
+
   useEffect(() => {
     console.log("isLoaded", isUserLoaded);
     console.log("userId", userId);
+
+    if (isUserLoaded && userId) {
+      fetchUser();
+    } else {
+      console.log("User is not logged in");
+    }
   }, [isUserLoaded, userId]);
 
   return (
